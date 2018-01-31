d792b7e66d08804b39d62cb5cd01bad4ddd0db80
Dmitry-Kiselev <kdem27@gmail.com>
2018-01-31 21:40:24 +0200

    add type annotations

--- cluster_tasks/__init__.py
+++ cluster_tasks/__init__.py
@@ -1,9 +1,9 @@
-def add(x ,y):
+def add(x:int ,y:int):
     print('Task add called with args: {}, {}'.format(x, y))
     return x + y
 
 
-def mul(x ,y):
+def mul(x:int ,y:int):
     print('Task mul called with args: {}, {}'.format(x, y))
     return x * y
 

4c40b83ba0f09917075159408695c3a54a2a4004
Dmitry-Kiselev <kdem27@gmail.com>
2018-01-31 21:40:12 +0200

    remove celery

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 from setuptools import setup
 
 setup(name='cluster_tasks',
-      version='0.2',
+      version='0.3',
       description='package with simple tasks',
       author='Dmitry Kiselev',
       author_email='kdem27@gmail.com',
       license='MIT',
       packages=['cluster_tasks'],
-      install_requires=[
-          'celery',
-      ],
       zip_safe=False)
